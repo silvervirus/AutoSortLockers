e07d3e2a1268ec5f7ab2e2a3a5f8a4cdd243136d
M3dicCookie <pred102@hotmail.com>
2024-03-03 11:31:16 -0800

    Delete UpdatedLegacyMods-master directory
    removing old code

--- UpdatedLegacyMods-master/AutosortLockers/obj/Subnautica/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace AutosortLockers
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "AutosortLockers";
-        public const string PLUGIN_NAME = "AutosortLockers";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
