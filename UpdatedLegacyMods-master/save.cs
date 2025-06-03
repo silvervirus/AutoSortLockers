61329b30118a968e2dc9dabe7bb8ca68e9f04984
M3dicCookie <pred102@hotmail.com>
2025-06-03 09:24:30 -0700

    Add files via upload

---
+++ UpdatedLegacyMods-master/save.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nautilus.Json;
+
+
+namespace DockedVehicleStorageAccess
+{
+    [Serializable]
+    public class SaveDataEntry
+    {
+        public string id;
+    }
+
+    [Serializable]
+    public class SaveData 
+    {
+        public List<SaveDataEntry> Entries = new List<SaveDataEntry>();
+    }
+}
